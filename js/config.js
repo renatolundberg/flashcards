7fb37fd0db8db5200cd137e4cb465135bbcb56f8
renato.lundberg <renato.lundberg@maps.com.br>
2026-08-25 17:56:11 -0300

    Configura credenciais públicas do Google (client id + API key)

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 export const DRIVE_CLIENT_ID = '1068137001911-0emb9v8ab2i4qtd467e799hdct993p81.apps.googleusercontent.com';
-export const DRIVE_API_KEY = '';
+export const DRIVE_API_KEY = 'AIzaSyDORP7WILtZVLnqK6LIK4yGUgkFd8UIBAM';
